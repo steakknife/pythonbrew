6f90c5159d6bc808edeafa73c3c2d22ca50a27c1
Barry Allard <barry@barryallard.name>
2012-02-10 21:46:54 -0800

    Added hash checking to Download

--- pythonbrew/downloader.py
+++ pythonbrew/downloader.py
@@ -14,10 +14,23 @@ def get_stable_version():
     return to_str(c.read(PYTHONBREW_STABLE_VERSION_URL).strip())
 
 class Downloader(object):
-    def download(self, msg, url, path):
+    def download(self, msg, url, path, _hash = None):
         logger.info("Downloading %s as %s" % (msg, path))
         c = Curl()
         c.fetch(url, path)
+        if _hash:
+            check_hash(self, msg, path, _hash)
+            
+    def check_hash(self, msg, path, _hash):
+        import hashlib
+        hasher = hashlib.sha()
+        with open(path,'rb') as f: 
+            for chunk in iter(lambda: f.read(8192), ''): 
+                hasher.update(chunk)
+        if hasher.hexdigest() != _hash:
+            import os
+            os.unlink(path)
+            raise Exception("Hash on %s failed (was %s, expected %s)" % (msg, hasher.hexdigest, _hash))
 
 def get_pythonbrew_update_url(version):
     if version == "master":
